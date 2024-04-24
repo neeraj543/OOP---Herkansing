2a6b5791962ad1634013e71808c0e2c1c788c020
Neeraj <r1008394@student.thomasmore.be>
2024-04-24 11:10:33 +0200

    Vraag 4

---
+++ src/ColoredItem.java
@@ -0,0 +1,19 @@
+public class ColoredItem implements IColor {
+
+    private Color color;
+
+
+    public ColoredItem() {
+        color = Color.NONE;
+    }
+
+    @Override
+    public Color getColor() {
+        return null;
+    }
+
+    @Override
+    public void paint(Color color) {
+
+    }
+}
